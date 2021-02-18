b4b67a84855394a11ed32be4d5ad3602b73040cd
RodrigoCury <rodrigocury@ufu.br>
2021-02-18 19:58:55 -0300

    Media_URL&ROOT created with staticfiles_DIRS

--- djangoBlog/settings.py
+++ djangoBlog/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'djangoBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,4 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = 'media'
+
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    'statics',
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
