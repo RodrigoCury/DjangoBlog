3058b507a3c41c3699e109d300f63b1f193e5e11
RodrigoCury <rodrigocury@ufu.br>
2021-02-18 19:57:08 -0300

    BlogDetailView Created

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import BlogPost
 
 # Create your views here.
+
+
+class BlogListView(ListView):
+    model = BlogPost
+    template_name = "blog/home.html"
+
+
+class BlogDetailView(DetailView):
+    model = BlogPost
+    template_name = "blog/detail.html"
+    context_object_name = 'blogpost'
