11c1712fa532aa62c9fe7fa3aa4a544780e0fb28
RodrigoCury <rodrigocury@ufu.br>
2021-02-18 19:58:08 -0300

    Published manager created

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User  # Link an User to a BlogPost
+from django.urls import reverse
 
 # Create your models here.
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset()\
+
 
 
 class BlogPost(models.Model):
@@ -21,6 +28,10 @@ class BlogPost(models.Model):
     status = models.CharField(max_length=10,
                               choices=STATUS,
                               default="rascunho")
+    publishedmanager = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.slug])
 
     class Meta:
         # "-MODEL_COLUMN" => ordem decrescente/ "MODEL_COLUMN" => ordem crescente
