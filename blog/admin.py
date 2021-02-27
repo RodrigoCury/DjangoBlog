90778c5bb96cbe3a48d105a7a6dfd0cfbeecbe13
RodrigoCury <rodrigocury@ufu.br>
2021-02-26 21:37:43 -0300

    imgs/postnames can be visualized on admin

--- blog/admin.py
+++ blog/admin.py
@@ -6,14 +6,21 @@ from .models import BlogPost, Category
 
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
-    list_display = ('title', "slug", "author", "status")
+    list_display = ('title', "slug", "author", "status",)
     list_filter = ('status', "created", "published", "author")
+    readonly_fields = ('visualize_img', )
     date_hierarchy = "published"
-    raw_id_fields = ("author",)
+    raw_id_fields = ("author", )
     search_fields = ('title', "content")
     prepopulated_fields = {
         "slug": ("title",)
     }
+
+    def visualize_img(self, obj):
+        return obj.view_image
+    visualize_img.short_description = "Imagem Cadastrada"
+    visualize_img.allow_tags = True
+
     """
 STATUS = (
         ('rascunho', "Rascunho"),
@@ -45,9 +52,20 @@ STATUS = (
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'published', 'created_at')
+    readonly_fields = ('related_posts', 'visualize_img')
     list_filter = ('name', 'published', 'created_at')
     date_hierarchy = 'published'
     search_fields = ('name', )
+
+    def related_posts(self, obj):
+        return [post.title for post in obj.get_posts.all()]
+    related_posts.short_description = 'Posts Relacionados'
+    related_posts.allow_tags = True
+
+    def visualize_img(self, obj):
+        return obj.view_image
+    visualize_img.short_description = "Imagem Cadastrada"
+    visualize_img.allow_tags = True
     '''
         name = models.CharField(max_length=100,
                             verbose_name='Nome')
