92fad245848f819bdbd51ea064cf1ef48058feff
RodrigoCury <rodrigocury@ufu.br>
2021-02-26 21:36:47 -0300

    Used Checkboxes for select Categorys

--- blog/forms.py
+++ blog/forms.py
@@ -1,12 +1,16 @@
 from ckeditor.widgets import CKEditorWidget
-from .models import BlogPost
+from .models import BlogPost, Category
 from django import forms
 
 
 class BlogPostForm(forms.ModelForm):
-    title = forms.CharField(max_length=50)
-    content = forms.CharField(widget=CKEditorWidget)
+    title = forms.CharField(max_length=50, label='Título')
+    category = forms.ModelMultipleChoiceField(
+        label='Categoria',
+        queryset=Category.objects.all(),
+        widget=forms.CheckboxSelectMultiple)
+    content = forms.CharField(widget=CKEditorWidget, label='Conteúdo',)
 
     class Meta:
         model = BlogPost
-        fields = '__all__'
+        fields = ('title', 'content', 'category', 'image', 'status')
